71ba73e627190a282349ba91653d1c3bc7832960
1976Aj <aj@arrayschool.com>
2018-01-16 10:45:50 -0700

    Updated formspree

--- js/agency.js
+++ js/agency.js
@@ -1,3 +1,19 @@
+// Formspree Redirect
+var message = "";
+ $("#sendMessage").on("click", function() {
+   message = $("#contactform").serialize();
+   $.ajax({
+     url: "//formspree.io/ajacknitsky@gmail.com",
+     method: "POST",
+     data: {message: message},
+     dataType: "json"
+   });
+   alert('Thanks for the email, we\'ll be in touch promptly.');
+   return false;
+ });
+
+
+
 (function($) {
   "use strict"; // Start of use strict
 
